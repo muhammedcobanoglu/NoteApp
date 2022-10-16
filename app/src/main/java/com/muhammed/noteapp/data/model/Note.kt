2781ec8ba7d196cb1ec0411876d719b4ba302118
muhammedcobanoglu <muhammed.cobanoglu@tsoft.com.tr>
2022-10-16 23:17:37 +0300

    update the column name and change the variable name as camel case

--- app/src/main/java/com/muhammed/noteapp/data/model/Note.kt
+++ app/src/main/java/com/muhammed/noteapp/data/model/Note.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "notes")
 data class Note(
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "note_id") var id: Int = 0,
+    @ColumnInfo(name = "id") var noteId: Int = 0,
     @ColumnInfo(name = "title") var title: String = "",
     @ColumnInfo(name = "description") var description: String = "",
     @ColumnInfo(name = "url") var url: String = "",
